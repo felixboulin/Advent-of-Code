5c4c1f523ea29c5c08a45d9a24b1d49b3aafa8e6
lixf2 <95010359+lixf2@users.noreply.github.com>
2022-12-06 16:50:39 +0100

    syntax cleaning

--- day1/A.py
+++ day1/A.py
@@ -1,7 +1,5 @@
 with open("input.txt") as f:
     data = f.read().splitlines()
-    elf_no = 1
-    best_elf = 1
     max_cal = 0
     cal = 0
 
@@ -9,11 +7,8 @@ with open("input.txt") as f:
         if e == "":
             if cal > max_cal:
                 max_cal = cal
-                best_elf = elf_no
             cal = 0
-            elf_no += 1
         else:
             cal += int(e)
 
-    print("Best elf is: ", best_elf)
     print("Max calories: ", max_cal)
